341e8750a63f4537489dac49b086614302e51e72
alhakimhanif30-lang <alhakimhanif30@gmail.com>
2026-05-24 21:07:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,72 @@
-*{box-shadow:0 0 30px rgba(0,217,255,0.4);
-  animation:floating 3s infinite;}
+window.addEventListener('load',()=>{
 
-@keyframes floating{0%,100%{transform:translateY(0)}
-   50%{transform:translateY(-10px)}}
+const particlesMesh = new THREE.Points(
+  particlesGeometry,
+  particlesMaterial
+)
 
-.sidebar{position:fixed;top:0;right:-400px;width:350px;height:100vh;z-index:200;padding:30px;transition:.5s;}
-.sidebar.active{right:0;}
+scene.add(particlesMesh)
+camera.position.z = 3
 
-footer{padding:80px;text-align:center;border-top:1px solid rgba(255,255,255,0.1);}
+function animate(){
+  requestAnimationFrame(animate)
 
-footer h2{color:#67e8f9;margin-bottom:20px;}
+  particlesMesh.rotation.y += 0.0008
+  particlesMesh.rotation.x += 0.0003
 
-@media(max-width:900px){
-  .hero,
-  .payment-section{grid-template-columns:1fr;}
+  renderer.render(scene,camera)
+}
 
-  .hero h1{font-size:4rem;}
+animate()
 
-  nav{padding:20px;}
+window.addEventListener('resize',()=>{
+  renderer.setSize(window.innerWidth,window.innerHeight)
+  camera.aspect = window.innerWidth/window.innerHeight
+  camera.updateProjectionMatrix()
+})
 
-  .nav-links{display:none;}
+const ctx = document.getElementById('analyticsChart')
 
-  .section,
-  .hero{padding:120px 20px;}
+new Chart(ctx,{
+  type:'line',
+  data:{
+    labels:['00','04','08','12','16','20'],
+    datasets:[{
+      label:'AI Infrastructure',
+      data:[12,19,14,25,22,30],
+      borderColor:'#00d9ff',
+      backgroundColor:'rgba(0,217,255,.1)',
+      tension:0.4,
+      fill:true
+    }]
+  },
+  options:{
+    plugins:{
+      legend:{
+        labels:{color:'white'}
+      }
+    },
+    scales:{
+      y:{
+        ticks:{color:'white'}
+      },
+      x:{
+        ticks:{color:'white'}
+      }
+    }
+  }
+})
+
+const cartBtn = document.getElementById('cartBtn')
+const wishlistBtn = document.getElementById('wishlistBtn')
+
+const cartSidebar = document.getElementById('cartSidebar')
+const wishlistSidebar = document.getElementById('wishlistSidebar')
+
+cartBtn.addEventListener('click',()=>{
+  cartSidebar.classList.toggle('active')
+})
+
+wishlistBtn.addEventListener('click',()=>{
+  wishlistSidebar.classList.toggle('active')
+})
